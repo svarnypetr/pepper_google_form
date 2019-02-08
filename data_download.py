e2b7716c24731d2d2e6b0da5776d9953d68be116
Hagen Lehmann <hagen.lehmann@gmail.com>
2019-02-08 16:21:12 +0100

    minimal example for macerata with Hagen

--- data_download.py
+++ data_download.py
@@ -6,8 +6,8 @@ import json
 
 SCOPE = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
-SECRETS_FILE = "southern-range-230110-e86cb31f6665.json"
-SPREADSHEET = "Hagen test form (sheet)"
+SECRETS_FILE = "macerata-1549040199941-9b1795f038ec.json"
+SPREADSHEET = "Lez 1 02 26 Obiettivi (Responses)"
 # Based on docs here - http://gspread.readthedocs.org/en/latest/oauth2.html
 # Load in the secret JSON key (must be a service account)
 json_key = json.load(open(SECRETS_FILE))
@@ -29,14 +29,14 @@ for sheet in gc.openall():
 # Rename the columns to make it easier to manipulate
 # The data comes in through a dictionary so we can not assume order stays the
 # same so must name each column
-        column_names = {'Time stamps': 'timestamp',
-                        'Question 1': 'question',
-                        }
-        data.rename(columns=column_names, inplace=True)
-        data.timestamp = pd.to_datetime(data.timestamp)
-        # import ipdb; ipdb.set_trace()
+        # column_names = {'Time stamps': 'timestamp',
+        #                 'Question 1': 'question',
+        #                 }
+        # data.rename(columns=column_names, inplace=True)
+        # data.timestamp = pd.to_datetime(data.timestamp)
+        # # import ipdb; ipdb.set_trace()
 
-        # pd.Timestamp.now()
-        # pd.date_range(pd.Timestamp.now(), periods=2, freq='1min')[1]
+        # # pd.Timestamp.now()
+        # # pd.date_range(pd.Timestamp.now(), periods=2, freq='1min')[1]
 
-        print(data.head())
+        # print(data.head())
