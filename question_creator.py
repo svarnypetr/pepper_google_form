3d1164d70bb151b8649e1a27bbd528be44decc6e
svarnypetr <svarnypetr@gmail.com>
2019-05-16 21:37:46 +0200

    generate proper order of questions also with total number of students that answered the test

--- question_creator.py
+++ question_creator.py
@@ -1,39 +1,63 @@
 class MyClass(GeneratedClass):
     def __init__(self):
         GeneratedClass.__init__(self)
+        self.position = -1
+        self._input_list = ""
+        self.answer = ""
 
 
     def onLoad(self):
         #put initialization code here
-        self.position = 0
-        self._input_str = ""
+        self.position = -1
+        self._input_list = []
         pass
 
     def onUnload(self):
         #put clean-up code here
-        self.position = 0
-        self._input_str = ""
+        self.position = "terminated"
+        self._input_list = []
         pass
 
+
     def code(self):
-        if self.position < len(self._input_str)-1:
-            self.counts(str(self._input_str[self.position]))
+        self.logger.info(self.position)
+
+        if self.position == -1:
+            # I add the last value from the list but divide it by 100, it were %
+            # I am using the format notation that is a little better and more versatile, read, google and learn
+            self.output_answer("Questions were answered by {} students".format(int(self._input_list[self.position])/100))
+            self.position = 0
+
+        # we now iterate through the list only until the before last member
+        elif self.position < len(self._input_list)-2:
+            self.answer = "was answered correctly by {} percent.".format(self._input_list[self.position])
             self.position += 1
-            self.question_count("Question " + str(self.position))
-        elif  self.position == len(self._input_str)-1:
-            self.onStopped(str(self._input_str[self.position]))
-            self.question_count("Overall ")
+            self.output_answer("Question " + str(self.position) + " " + self.answer)
+
+        elif self.position == len(self._input_list)-2:
+            self.answer = "the answers were on {} percent correct.".format(self._input_list[self.position])
+            self.onStopped()
+            self.output_answer("Overall " + self.answer)
+
         else:
             pass
 
 
     def onInput_onString(self, _input_str):
-        self._input_str = _input_str
+        """
+        We need to split the input string based on the percent separator
+        '2%2%'.split('%') this split leads to the list ['2', '2', ''],
+        therefore we remove the last member right-away by saying "all except the last one" -> [:-1]
+        """
+        processed_string = _input_str.split('%')[:-1]
+        self._input_list = processed_string
+        self.position = -1
         self.code()
         pass
 
     def onInput_input(self):
-        self.code()
+        if not self.position == "terminated":
+            self.code()
         pass
 
     def onInput_onStop(self):
