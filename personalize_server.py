cfdd505b8e9b075042f0c1548f832a5c00dcd5e4
svarnypetr <svarnypetr@gmail.com>
2020-03-02 10:26:01 +0100

    working test on lez11

--- personalize_server.py
+++ personalize_server.py
@@ -13,7 +13,7 @@ SCOPE = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
 API_KEY_FILE = "key.json"
-SPREADSHEET = "pepper competenza 2 (Responses)"
+SPREADSHEET = "Lez11 (Responses)"
 PORT = 6557  # Make sure it's within the > 1024 $$ <65535 range
 
 
