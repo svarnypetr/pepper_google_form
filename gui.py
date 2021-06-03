db3e80ae9df4d9481cd2a1302365338ee2c229bb
svarnypetr <svarnypetr@gmail.com>
2021-06-03 15:57:38 +0200

    Setup basic persistent window

--- gui.py
+++ gui.py
@@ -1,26 +1,24 @@
 import PySimpleGUI as sg
 
-<<<<<<< 7259359eae525fda1aea8f95a7bb5970c4c02039
-# TODO: based on https://pypi.org/project/PySimpleGUI/ or https://pypi.org/project/PySimpleGUI/
-=======
-# TODO: based on https://pypi.org/project/PySimpleGUI/
->>>>>>> Add a very primitive GUI for outset
 # TODO: add also a dropdown for the available matricolas
 # TODO: incorporate all the code and dropdown for the various approaches - prof etc.
 
 # Define the window's contents
 layout = [[sg.Text("What folder should be used?")],     # Part 2 - The Layout
           [sg.Input()],
-          [sg.Button('Ok')]]
+          [sg.Button('Ok'), sg.Button('Exit')]]
 
 # Create the window
 window = sg.Window('Pepper edu program', layout)      # Part 3 - Window Definition
 
-# Display and interact with the Window
-event, values = window.read()                   # Part 4 - Event loop or Window.read call
-
-# Do something with the information gathered
-print('Hello', values[0], "! Thanks for trying PySimpleGUI")
+while True:  # Event Loop
+    event, values = window.read()
+    print(event, values)
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+    if event == 'Show':
+        # Update the "output" text element to be the value of "input" element
+        window['-OUTPUT-'].update(values['-IN-'])
 
 # Finish up by removing from the screen
 window.close()
